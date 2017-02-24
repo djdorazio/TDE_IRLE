fixes to IRLCpoint and addition of averaged source points with better source fit

FluxFuncs_TDEs.py
@@ -20,7 +20,7 @@ import scipy.signal as sgn
 #### INTEGRATION ERROR TOLS
 myrel = 1.e-8
 myabs = 1.e-8#1.e-60
-reclim = 4#100
+reclim = 1#100
 limlst = 1
 maxp1 = 1
 fo = 1
@@ -160,7 +160,7 @@ def tauObs(nu, x, y, z, Rout, aeff, n0, Rd, p, thetT, JJ, nu0, nn):
 def Fsrc_Anl(t, r, Lavg, tfb, t0, FQfac):
 	F0 = Lavg/(4.*ma.pi*r*r)
 	FQ = F0/100.#FQfac
-	gam = 2.0
+	gam = 2.3
 
 
 	if ((t-t0)<=0.0):

MCMC_IR.py
@@ -34,7 +34,7 @@ Fit_IR = True
 QvFit = False
 
 ##multiprocessing
-NThread = 32
+NThread = 2
 
 
 ##Trim beginning of Vband
@@ -91,6 +91,54 @@ V_Gal = 17.6#mean(V_srtE)
 
 # magVnet = -2.5*np.log10(10.**(-V_srtE/2.5) - 10.**(-V_Gal/2.5))
 # V_srt = magVnet
+
+
+
+
+###### get average value for each cluster of data points in time
+iseg = []
+iseg.append(-1)
+for i in range(len(tV_srt)-1):
+	if (abs((tV_srt[i] - tV_srt[i+1])) > 100 ):
+		iseg.append(i)
+iseg.append(len(tV_srt)-1)
+
+tV_avg = []
+V_avg = []
+V_avsg = []
+
+#VarMn_test_W1 = []
+#VarMn_test_W2 = []
+
+
+for i in range(0 , len(iseg)-1):
+	tV_avg.append(np.mean(tV_srt[iseg[i]+1:iseg[i+1]+1]))
+
+	V_avg.append(np.mean(V_srt[iseg[i]+1:iseg[i+1]+1]))
+	
+
+	
+	Nseg = len(V_sigsrt[iseg[i]+1:iseg[i+1]]) + 1
+
+
+	V_avsg.append(np.sqrt(sum( (V_sigsrt[iseg[i]+1:iseg[i+1]])**2 )/Nseg  ))
+
+
+	# ## see if variance of means or mean of variances is larger
+	# VarMn_test_W1.append(np.mean(W1_sig[iseg[i]+1:iseg[i+1]+1])/np.std(W1_mag[iseg[i]+1:iseg[i+1]+1]))
+	# VarMn_test_W2.append(np.mean(W2_sig[iseg[i]+1:iseg[i+1]+1])/np.std(W2_mag[iseg[i]+1:iseg[i+1]+1]))
+
+
+tV_avg = np.array(tV_avg)
+V_avg = np.array(V_avg)
+V_avsg = np.array(V_avsg)
+
+
+
+
+
+
+
 
 
 ###############################
@@ -209,11 +257,12 @@ if (Fit):
 		##########################
 
 		Shell_File = "_Fit_Vband_"
-		param_names = [r"$L_0$",r"$t_0$",r"$t_{fb}$", r"$\gamma$", r"$FQ_{\rm{V}}$"]
-		p0V = [Lav/10.**45, t0/yr2sec, tfb/yr2sec, 1.0, 100.0]
+		param_names = [r"$L_0$",r"$t_0$",r"$t_{fb}$", r"$\gamma$"]
+		p0V = [0.7, t0/yr2sec, tfb/yr2sec, 2.3]
 		ndim = len(p0V)
 		nwalkers = ndim*4
-		V_sampler  = emcee.EnsembleSampler(nwalkers, ndim, ln_V_posterior, threads=NThread,args=(tV_srt, Varg, V_srt, V_sigsrt))
+		#V_sampler  = emcee.EnsembleSampler(nwalkers, ndim, ln_V_posterior, threads=NThread,args=(tV_srt, Varg, V_srt, V_sigsrt))
+		V_sampler  = emcee.EnsembleSampler(nwalkers, ndim, ln_V_posterior, threads=NThread,args=(tV_avg, Varg, V_avg, V_avsg))
 
 
 
@@ -223,7 +272,7 @@ if (Fit):
 		V_walker_p0 = np.random.normal(V_p0, np.abs(V_p0)*1E-3, size=(nwalkers, ndim))
 
 					
-		clen = 32#4048
+		clen = 1024#4048
 		V_pos,_,_ = V_sampler.run_mcmc(V_walker_p0, clen)
 
 
@@ -371,7 +420,7 @@ if (Fit):
 		param_names = [r"$\eta_R$",r"$\cos{\theta_T}$",r"$\sin(J)$", r"$\mu\rm{m}\nu_0c^{-1}$", r"$L_{45}$"]
 		p0IR = [etaR, np.cos(thetTst), np.sin(JJt), nu0/numicron, Lav/10.**45]
 		ndim = len(p0IR)
-		nwalkers = ndim*8
+		nwalkers = ndim*6
 
 		IR_sampler  = emcee.EnsembleSampler(nwalkers, ndim, ln_IR_posterior, threads=NThread,args=(t_avg, argW1, argW2, RHS_table, T_table, W1_avg, W1_avsg, W2_avg, W2_avsg))
 
@@ -519,18 +568,18 @@ if (Fit):
 if (Pplot):
 	### PLOT POINTS
 	print "PLOTTING"
-	Nt=40
+	Nt=20
 	tt = np.linspace(0.00, 12.,       Nt)*tfb
 
 	if (Fit==False):
 		#IR_p_opt = [etaR, np.cos(thetTst), np.sin(JJt), 100.0]
 		IR_p_opt = [etaR, np.cos(thetTst), np.sin(JJt), nu0/numicron, 1.0]
-		V_p_opt = [Lav/10.**45, t0/yr2sec, tfb/yr2sec, 2.0, 100.7]
+		V_p_opt = [0.7, t0/yr2sec, tfb/yr2sec, 2.3]
 	if (Fit_IR==False):
 		#IR_p_opt = [etaR, np.cos(thetTst), np.sin(JJt), 100.0]	
 		IR_p_opt = [etaR, np.cos(thetTst), np.sin(JJt), nu0/numicron, 1.0]
 	if (Fit_Src==False):
-		V_p_opt = [Lav/10.**45, t0/yr2sec, tfb/yr2sec, 1.8, 100.]
+		V_p_opt = [Lav/10.**45, t0/yr2sec, tfb/yr2sec, 1.8]
 
 	FsrcI1 = np.zeros(Nt)
 	FVplus = np.zeros(Nt)
@@ -562,8 +611,12 @@ if (Pplot):
 	s1 = plt.plot(tt/(tfb)*365., FsrcI1-3.5, linestyle = '--', color='black', linewidth=3)
 
 
-	Vdat   = plt.errorbar(tV_srt, V_srt-3.5, yerr=V_sigsrt, linestyle="none", color='blue', alpha=1., elinewidth=1.5)
-	Vsct   = plt.errorbar(tV_srt, V_srt-3.5, color='blue', alpha=1.)
+	Vdat   = plt.errorbar(tV_srt, V_srt-3.5, yerr=V_sigsrt, linestyle="none", color='blue', alpha=0.2, elinewidth=1.5)
+	Vsct   = plt.scatter(tV_srt, V_srt-3.5, color='blue', alpha=0.2)
+
+	Vavg     = plt.errorbar(tV_avg, V_avg-3.5, yerr=V_avsg, linestyle="none", color='black', alpha=1., elinewidth=1.5)
+	Vavsct   = plt.scatter(tV_avg, V_avg-3.5, color='black', alpha=1.)
+
 
 	#Vav   = plt.errorbar(t_avg, W1_avg, yerr=W1_avsg, linestyle="none", color='black', alpha=1., elinewidth=1.5)
 

emcee_Funcs_TDEs.py
@@ -27,22 +27,22 @@ def ln_IR_prior(params):
 
 def ln_V_prior(params):
 	#sinJJ, cosTT, Rin, alpha = params
-	L0, t0, tfb, gam, FQfac = params
+	L0, t0, tfb, gam = params
 					
-	if L0 < 0.0001 or L0 >  2.0:  ##in units of 10^45 erg/s  bol
+	if L0 < 0.0001 or L0 >  10.0:  ##in units of 10^45 erg/s  bol
 		return -np.inf
 
 	if t0 < 50./365. or t0 > 200./365.: ##dont shift more than 4 years
 		return -np.inf
 					
-	if tfb < 0.0 or tfb > 2.:
+	if tfb < 0.0 or tfb > 4.:
 		return -np.inf
 
-	if gam < 0.5 or gam > 3.0:
+	if gam < 0.1 or gam > 4.0:
 		return -np.inf
 
-	if FQfac <= 90.0 :
-		return -np.inf
+	# if FQfac <= 90.0 :
+	# 	return -np.inf
 			
 	return 0.
 
@@ -60,7 +60,7 @@ def IRLC_point(p, t, args, RHStable, Ttable):
 	etaR, cosT, JJt, nu0, Lav = p
 	FQfac = 100.0
 	nu0=nu0*numicron
-	Lav = Lav*10.**(46)
+	Lav = Lav*10.**(45)
 	#Rde = Rde*pc2cm
 	thetTst = np.arccos(cosT)
 	JJt = np.arcsin(JJt)
@@ -98,18 +98,19 @@ def ln_V_likelihood(p, t, arg, y, dy):
 
 
 def VLC_point(p, t, arg, LavIRfit):
-	L0, t0, tfb, gam, FQfac = p
+	L0, t0, tfb, gam = p
+	FQfac = 100.0
 	L0 = L0*10.**45
 	t0 = t0*yr2sec
 	tfb = tfb*yr2sec
 	Dst, Frel, FV_gal = arg
-	BC = 12.*5./2./LavIRfit ## bol corr to Vband
+	BC = 12.*5./2./LavIRfit ## bol corr to Vband - doesnt matter for fits, jsut makes Vband correct mag in end
 	return -2.5*np.log10(( Fsrc_Anl_Fit(t, Dst, L0, tfb, t0, gam, FQfac)/BC + FV_gal)/Frel)
 
 
 def VTDE_Err2(p, t, arg, y, dy):
 	print "EVAL", p
-	chi = (y - np.minimum(VLC_point(p, t, arg),17.6) )/ dy
+	chi = (y - np.minimum(VLC_point(p, t, arg, 1.0),17.6) )/ dy
 	chi2 = sum(chi*chi) 
 	print(chi2)
 	return chi2